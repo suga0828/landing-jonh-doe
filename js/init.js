be9b434a8d4fd68c84a1bd30e656d0c82797f8d2
Alexander <alexander.sf0828@gmail.com>
2017-12-15 14:14:05 -0400

    Cambio de estilos

--- js/init.js
+++ js/init.js
@@ -1,14 +1,21 @@
-(function($){
-  $(function(){
+/* 
+ _____   _           _         _                        _                  
+|_   _| | |         | |       | |                      | |                 
+  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
+  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
+ _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
+ \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+Oh nice, welcome to the js file of dreams.
+Enjoy responsibly!
+@ihatetomatoes
 
-	$(".page-loader").css( "display", "none")
-	$('.scrollspy').scrollSpy({
-		scrollOffset : 0,
- 		throttle : 100, //-> scrollspy throttling.
-	});
+*/
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function() {
+	
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 1000);	
+	
+});
